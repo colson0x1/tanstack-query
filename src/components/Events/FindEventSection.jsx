41a2adbb75e00b1b7b23876cb85a8ba1cde9612f
Colson <colsoncalifornication@gmail.com>
2024-05-05 13:05:03 +0545

    implement dynamic query functions and query keys

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,26 +1,68 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import { fetchEvents } from '../../util/http';
+import LoadingIndicator from '../UI/LoadingIndicator';
+import ErrorBlock from '../UI/ErrorBlock';
+import EventItem from './EventItem';
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // make sure both fetchEvents and queryKey are updated dynamically and lead
+  // to different query being sent as this search term changes
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', { search: searchTerm }],
+    queryFn: () => fetchEvents(searchTerm),
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSearchTerm(searchElement.current.value);
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title='An error occurred'
+        message={error.info?.message || 'Failed to fetch events.'}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className='events-list'>
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 
   return (
-    <section className="content-section" id="all-events-section">
+    <section className='content-section' id='all-events-section'>
       <header>
         <h2>Find your next event!</h2>
-        <form onSubmit={handleSubmit} id="search-form">
+        <form onSubmit={handleSubmit} id='search-form'>
           <input
-            type="search"
-            placeholder="Search events"
+            type='search'
+            placeholder='Search events'
             ref={searchElement}
           />
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -6,6 +6,24 @@ import EventItem from './EventItem.jsx';
 import { fetchEvents } from '../../util/http.js';
 
 export default function NewEventsSection() {
+  // All useQuery wants is a fn (i.e queryFn) that returns a Promise
+  // THat is its requirement here
+  // fetchEvents will be executed by TanStack Query to fetch our data
+  // When using useQuery, every fetch http request (GET) that we send in the
+  // end also should have a queryKey which will then internally be used by
+  // React Query i.e by TanStack Query, to cache the data that's yielded by
+  // that request so that the 'response' from that request could be reused in
+  // the future if we're trying to send the same request again.
+  // i.e Data can be shown to the user quicker if we already have it because
+  // it doesn't need to be refetched all the time
+  // So that's why every query needs such a key and that key is actually an
+  // array. An array of values which are then internally stored by React Query
+  // such that whenever we're using a similar array or similar values, React Query
+  // sees that and is able to reuse existing data.
+  // We can have strings, objects, nested arrays or other kind of values
+  // And now we get something back from useQuery and that is an object
+  // queryObject but we can use destructuring to pull out the elements that
+  // are most important to us.
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events'],
     queryFn: fetchEvents,
